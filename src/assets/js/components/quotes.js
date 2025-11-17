96be6596dd77a8e1912c114c4de06c14a8336593
Jérôme Coupé <jercoupe@gmail.com>
2025-11-17 18:14:14 +0100

    Quotes: round 1

--- src/assets/js/components/quotes.js
+++ src/assets/js/components/quotes.js
@@ -1,15 +1,43 @@
 const allQuotes = [
   {
-    text: "Une petite quote pour la route",
-    author: "Jean-Claude Vandamme",
+    text: "Un multimédia président du CEDI ... ce n'est que le début!",
+    author: "Christophe Pesesse",
   },
   {
-    text: "Avec ce que j'ai pris comme rayons, je peux donner cours de VFX",
+    text: "Dites, comment on fait pour le bar sans Jeremy et Omar?",
     author: "Jérôme Coupé",
   },
   {
-    text: "Finalement, il a seulement fallu 412 jours pour avoir les télés à Athéna.",
+    text: "Mardi passé, j'ai failli dessiner sur une des télés d'Athéna.",
     author: "Julien Moreau",
+  },
+  {
+    text: "J'ai beau avoir fait mon master à Jacquard, ils me laissent entrer chaque année.",
+    author: "Un traître quand même",
+  },
+  {
+    text: "C'est sans doute ce nouveau cours mais les MM1B sont vraiment jeunes, non ?",
+    author: "Cédric Deru",
+  },
+  {
+    text: "Avoir tes anciens profs comme collègues ça ne te rajeuni pas.",
+    author: "Thomas Scourneau",
+  },
+  {
+    text: "Il ne faut jamais vendre la peau de Blender avant d'avoir tué Maya.",
+    author: "Fabrice Laroche",
+  },
+  {
+    text: "Nuke c'est pas hyper sympa comme nom quand on y pense ...",
+    author: "Caroline Voglaire",
+  },
+  {
+    text: "Oui, Jérôme, je rentre sur Bruxelles cette année aussi ...",
+    author: "Nicolas Gillard",
+  },
+  {
+    text: "Pour rester zen, le CBD à la pause, y a que ça de vrai.",
+    author: "Rémi Vandenitte",
   },
 ];
 
