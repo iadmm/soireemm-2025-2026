06d7e0df85f334ab8e708aaaa0bcff1cf028b2a8
Jérôme Coupé <jercoupe@gmail.com>
2025-11-17 18:31:56 +0100

    Add open graph tags

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,7 +1,8 @@
 export default {
   metaTitle: "Soirée métiers multimédia 2025-2026",
   metaDescription: "Quelques anciennes et anciens viennent raconter leurs parcours scolaires et professionnels",
-  url: "https://metiers.iadmm.be",
+  metaImage: "socialmedia.jpg",
+  url: "https://soireemm.netlify.app/",
   registrations: true,
   registrationsUrl: "https://www.iad-arts.be",
   eventDate: "2026/02/03",
